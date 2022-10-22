699852bd6e0ddd218c2b710781315a0d0ac95bc6
AlexanderPanchyshyn <alexandr.panchyshyn34@gmail.com>
2022-10-22 15:58:51 +0200

    working on Object to Json

--- src/main/java/org/yellowteam/Main.java
+++ src/main/java/org/yellowteam/Main.java
@@ -1,6 +1,7 @@
 package org.yellowteam;
 
 import org.yellowteam.mapper.JavaJsonMapper;
+import org.yellowteam.models.Author;
 import org.yellowteam.models.Book;
 import org.yellowteam.models.BookShelf;
 
@@ -8,12 +9,19 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IllegalAccessException {
-        Book book1 = new Book("Blade Runner", 2022, Arrays.asList("Marley", "Bob"), true);
-        Book book2 = new Book("Bible", 1024, Arrays.asList("God", "Jesus"), false);
+        Author author1 = new Author("John Doe", 56);
+        Author author2 = new Author("Rebekka Mayor", 37);
+
+        Book book1 = new Book("Blade Runner", 2022, Arrays.asList("Marley", "Bob"), true, author1);
+        Book book2 = new Book("Bible", 1024, Arrays.asList("God", "Jesus"), false, author2);
+
         BookShelf bookShelf = new BookShelf(20, 50, Arrays.asList(book1, book2));
+
         JavaJsonMapper mapper = new JavaJsonMapper();
         var jsonBook = mapper.toJson(bookShelf);
-        System.out.println(jsonBook);
+        System.out.println(bookShelf.getClass().toString());
+
+//        System.out.println(jsonBook);
     }
 }
 
--- src/main/java/org/yellowteam/mapper/JavaJsonMapper.java
+++ src/main/java/org/yellowteam/mapper/JavaJsonMapper.java
@@ -7,36 +7,54 @@ public class JavaJsonMapper implements JavaJsonMapperInterface {
 
     @Override
     public String toJson(Object o) throws IllegalAccessException {
-        return convertToJson(o);
+        return process(o);
     }
 
-    private String convertToJson(Object o) throws IllegalAccessException {
-        String json = "{\n";
+    private String process(Object o) throws IllegalAccessException {
+        String json = "{";
         List<String> list = new ArrayList<>();
-        var fields = o.getClass().getDeclaredFields();
 
-        for (var field : fields) {
-            field.setAccessible(true);
-            String name = field.getName();
-
-            if (field.get(o) instanceof List<?> fieldList) {
-                convertNonPrimitiveToJson(name, fieldList, list);
-            } else {
-                convertPrimitiveToJson(name, field.get(o), list);
-            }
-        }
+        convertToJson(o, list);
 
         json = collectValues(json, list);
 
         return json;
     }
 
+    private void convertToJson(Object o, List<String> list) throws IllegalAccessException {
+        var fields = o.getClass().getDeclaredFields();
+
+        for (var field : fields) {
+            field.setAccessible(true);
+            String name = field.getName();
+            var el = field.get(o);
+
+            if (el instanceof String ||
+                    el instanceof Character ||
+                    el instanceof Number ||
+                    el instanceof Boolean) {
+                convertPrimitiveToJson(name, field.get(o), list);
+            } else if (field.get(o) instanceof List<?> fieldList) {
+                convertNonPrimitiveToJson(name, fieldList, list);
+            } else {
+                convertObjectToJson(name, el, list);
+            }
+        }
+    }
+
+
+    private void convertObjectToJson(String objName, Object obj, List<String> list) throws IllegalAccessException {
+        list.add("\"%s\":{".formatted(objName));
+        convertToJson(obj, list);
+//        list.add("}");
+    }
+
     private String collectValues(String json, List<String> list) {
         for (int i = 0; i < list.size(); i++) {
-            var pos = i == list.size() - 1 ? "\n" : ",\n";
+            var pos = i == list.size() - 1 ? "" : ",";
             json += list.get(i) + pos;
         }
-        return json + "}\n";
+        return json + "}";
     }
 
     private void convertNonPrimitiveToJson(String name, List<?> fieldList, List<String> list) throws IllegalAccessException {
@@ -46,7 +64,7 @@ public class JavaJsonMapper implements JavaJsonMapperInterface {
             var pos = i == fieldList.size() - 1 ? "" : ",";
             val = typeChecker(el, val, pos);
         }
-        list.add("\t\"%s\" : [%n\t%s]".formatted(name, val));
+        list.add("\"%s\":[%s]".formatted(name, val));
     }
 
     private String typeChecker(Object el, String val, String pos) throws IllegalAccessException {
@@ -56,13 +74,13 @@ public class JavaJsonMapper implements JavaJsonMapperInterface {
                 el instanceof Boolean) {
             val += convertPrimitiveValueToJson(el) + pos;
         } else {
-            val += convertToJson(el) + pos;
+            val += process(el) + pos;
         }
         return val;
     }
 
     private void convertPrimitiveToJson(String name, Object obj, List<String> list) {
-        list.add("\t\"%s\" : \"%s\"".formatted(name, String.valueOf(obj)));
+        list.add("\"%s\":\"%s\"".formatted(name, String.valueOf(obj)));
     }
 
     private String convertPrimitiveValueToJson(Object element) {
