0624431b4c37bd3ba3a82c5aa43370d9824fb750
AlexanderPanchyshyn <alexandr.panchyshyn34@gmail.com>
2022-11-01 15:10:55 +0100

    working on conflicts 2.0

--- src/main/java/org/yellowteam/models/Book.java
+++ src/main/java/org/yellowteam/models/Book.java
@@ -1,6 +1,5 @@
 package org.yellowteam.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.util.List;
@@ -8,14 +7,9 @@ import java.util.List;
 @AllArgsConstructor
 @Getter @Builder
 public class Book {
-    @JsonProperty("Title")
     private String title;
-    @JsonProperty("Year")
     private int year;
-    @JsonProperty("Characters")
     private List<String> characters;
-    @JsonProperty("IsOriginalEdition")
     private boolean isOriginalEdition;
-    @JsonProperty("Author")
     private Author author;
 }
